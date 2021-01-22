241934a1fcafc460f3fdf4819edd98bc4346c03a
Jihee Ryu <chrisjihee@etri.re.kr>
2021-01-22 10:10:53 +0900

    processing dataset!

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import tensorflow_datasets as tfds
-import tensorflow_datasets_ko.text.c4ko
 import sys
 
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow.python.ops.gen_dataset_ops import PrefetchDataset
+
 if __name__ == '__main__':
+    # print('가')
+    # print(type('가'.encode('utf-8')))
+    #
+    # exit(1)
     if len(sys.argv) < 2 or sys.argv[1] not in ['gcs', 'local']:
         print("usage: python main.py [by]")
         print("       - by: 'gcs' or 'local'")
@@ -12,7 +18,14 @@ if __name__ == '__main__':
         tfds.load("c4ko/default", try_gcs=True)
 
     elif sys.argv[1] == 'local':
-        tfds.load("c4ko/default", try_gcs=False)
+        x, i = tfds.load("c4ko/default", with_info=True)
+        # print(f'i={i}')
+        print(f'x={x.keys()}')
+        dataset: PrefetchDataset = x['train']
+        # dataset: PrefetchDataset = x['validation']
+        for data in list(dataset):
+            print('-' * 120)
+            print(data['text'].numpy().decode("utf-8"))
 
     else:
         print("usage: python main.py [by]")
